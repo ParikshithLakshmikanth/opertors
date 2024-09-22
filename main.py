91bc398acc869161c4c672bcd1e574b6cc8f4b57
ParikshithLakshmikanth <132085131+ParikshithLakshmikanth@users.noreply.github.com>
2024-09-22 17:58:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+num1 = 10
+num2 = 5
+
+print("number1",num1)
+print("number2",num2)
+print("ADD",num1+num2)
+print("SUB",num1-num2)
+print("Prod",num1*num2)
+print("DIV",num1/num2)
+print("floordiv",num1//num2)
+print("MOD",num1%num2)
+print("SQ",num2**2)
+print("SQRT",num1**0.5)
+
+print("Equal",num1==num2)
+print("number1 > number2",num1>num2)
+print("number1 < number2",num1<num2)
+print("number1 NE number2",num1!=num2)
+
+result = num1/2 + num2**2+10
+print("Result of eqn",result)
